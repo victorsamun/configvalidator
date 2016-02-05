b03a1704a45c6939bb951e0a8534b21bc5bee89c
Victor Samun <victor.samun@gmail.com>
2016-02-05 17:48:09 +0500

    + setuptools support

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name='configvalidator',
+    version='0.9',
+    description='INI-config validator',
+    url='https://github.com/victorsamun/configvalidator',
+    author='Samun Victor',
+    author_email='victor.samun@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    packages=find_packages()
+)
